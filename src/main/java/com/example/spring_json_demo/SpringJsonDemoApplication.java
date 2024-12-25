67897a6a0eea80adf12f8be4fcc7ec54a6d88087
vlad <eremin.vlad@icloud.com>
2024-12-25 15:04:55 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_json_demo/SpringJsonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_json_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJsonDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJsonDemoApplication.class, args);
+	}
+
+}
